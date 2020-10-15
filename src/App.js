8100bdb869c261d7ef454268cdf8473408aae92c
yoru-san <cecel31@free.fr>
2020-10-15 11:24:41 +0200

    ajout de grommet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Grommet, Header, Button, Menu } from 'grommet';
+import { Home } from 'grommet-icons';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
 
 function App() {
+
   return (
-    <div className="App">
-    </div>
+    <Grommet theme={theme}>
+      <Header background="brand">
+        <Button icon={<Home />} hoverIndicator />
+        <Menu label="Accueil" items={[{ label: 'Présentation' }]} />
+      </Header>
+    </Grommet>
   );
 }
 
