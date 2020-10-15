92b30082119d6b6042daf59fd0b8adae9d85af76
yoru-san <cecel31@free.fr>
2020-10-15 14:31:44 +0200

    ajout feature

---
+++ src/features/experience/Experience.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Header, Button, Menu, Text, Box } from 'grommet';
+import { Home } from 'grommet-icons';
+
+const Experience = (props) => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <Box></Box>
+    );
+}
+
+export default Experience;
---
+++ src/features/presentation/Presentation.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Header, Button, Menu, Text, Box } from 'grommet';
+import { Home } from 'grommet-icons';
+
+const Presentation = (props) => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <Box></Box>
+    );
+}
+
+export default Presentation;
